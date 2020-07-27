ce61ee8bffb226218bcacc8294b0002f574e24fc
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-07-27 12:00:38 -0500

    attempted to fix all duplicate code

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -28,31 +28,37 @@ eventHub.addEventListener("crimeSelected", (crimeSelectedEvent) => {
                 return foundCrimeObj.name === currentCriminalObj.conviction
             }
         )
+render (filteredCriminals)
 
+
+})
+
+const render = (arrayOfCriminals) => {
     let criminalHTML = ""
     filteredCriminals.forEach(criminal => {
         criminalHTML += criminalHTMLConverter(criminal)
     })
-
+    
     contentTarget.innerHTML = `
     <h2>Glassdale Convicted Criminals</h2>
     <article class="criminalList">
     ${criminalHTML}
     </article>`
-
-})
+    
+ /*   let criminalHTMLRepresentation = ""
+    arrayOfCriminals.forEach(criminal => {
+        criminalHTMLRepresentation += criminalHTMLConverter(criminal)
+    }) 
+contentTarget.innerHTML = criminalHTMLRepresentation 
+} */
 
 export const criminalList = () => {
     
     getCriminals()
         .then(() => {
             const criminalArray = useCriminals()
-            let criminalHTMLRepresentation = ""
-            criminalArray.forEach(criminal => {
-                criminalHTMLRepresentation += criminalHTMLConverter(criminal)
-            })
-
-        contentTarget.innerHTML = criminalHTMLRepresentation
+            render(criminalArray)
+            
     })
 
 }
