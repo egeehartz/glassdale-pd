612e4b368bac7780f7ebee66bf8941617d68f66b
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-08-17 10:28:34 -0500

    added delete note functionality

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,3 +1,18 @@
+import {deleteNote} from "./NoteDataProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("noteButton--")) {
+        const [ prompt, noteIdString ] = clickEvent.target.id.split("--")  // "3"
+
+        deleteNote(noteIdString)
+    }
+})
+
+
+
+
 export const noteHTMLConverter = (noteObject, criminalObj) => {
     return `
         <section class="note">
@@ -6,6 +21,7 @@ export const noteHTMLConverter = (noteObject, criminalObj) => {
             <div class="note--content">Notes: ${noteObject.content}</div>
             <div class="note--author">Author: ${noteObject.author}</div>
             <div class="note--timestamp">Date: ${new Date(noteObject.timestamp).toLocaleDateString('en-US')}</div>
+            <button id="noteButton--${noteObject.id}">Delete</button>
         </section>
     `
 }
